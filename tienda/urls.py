9a440f4bf7f03a27666e9bd5d791f93d14d88293
gino <ginocandiotto10@gmail.com>
2026-07-30 18:57:18 +0000

    Agregue una ventana de inicio con bienvenida para cuando arrancamos el servidor

--- tienda/urls.py
+++ tienda/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from productos.views import inicio
+
 urlpatterns = [
+    path('', inicio, name='inicio'),
     path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
